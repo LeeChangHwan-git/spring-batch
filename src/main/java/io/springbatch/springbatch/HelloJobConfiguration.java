4f92de3e71544645aac817d0fde2ea30298e02cb
LeeChangHwan-git <chlee.github@gmail.com>
2024-04-08 17:28:28 +0900

    first commit

---
+++ src/main/java/io/springbatch/springbatch/HelloJobConfiguration.java
@@ -0,0 +1,70 @@
+package io.springbatch.springbatch;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
+
+@Configuration
+public class HelloJobConfiguration {
+
+
+    private final JobRepository jobRepository;
+    private final PlatformTransactionManager transactionManager;
+
+    @Autowired
+    public HelloJobConfiguration(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
+        this.jobRepository = jobRepository;
+        this.transactionManager = transactionManager;
+    }
+
+    @Bean
+    public Job helloJob(Step helloStep1, Step helloStep2) {
+        return new JobBuilder("helloJob", jobRepository)
+                .start(helloStep1)
+                .next(helloStep2)
+                .build();
+    }
+
+    @Bean
+    public Step helloStep1() {
+        return new StepBuilder("helloStep1",jobRepository)
+                .tasklet(new Tasklet() {
+                    @Override
+                    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+
+                        System.out.println("==========================");
+                        System.out.println(">> Hello Spring Batch!!");
+                        System.out.println("==========================");
+
+                        return RepeatStatus.FINISHED;
+                    }
+                }, transactionManager)
+                .build();
+    }
+    @Bean
+    public Step helloStep2() {
+        return new StepBuilder("helloStep2",jobRepository)
+                .tasklet(new Tasklet() {
+                    @Override
+                    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+
+                        System.out.println("==========================");
+                        System.out.println(">> Step2 Started");
+                        System.out.println("==========================");
+
+                        return RepeatStatus.FINISHED;
+                    }
+                }, transactionManager)
+                .build();
+    }
+}
